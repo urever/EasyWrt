e1e7e0f9c45e31ce46e8ba056ea2dfc12b968ece
urever <83172845+urever@users.noreply.github.com>
2023-11-18 00:11:22 +0800

    Update op1.sh

--- diy/mt798x/op1.sh
+++ diy/mt798x/op1.sh
@@ -4,3 +4,4 @@
 #=================================================
 ##添加自己的插件库
 echo -e "\nsrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk" >> feeds.conf.default
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
