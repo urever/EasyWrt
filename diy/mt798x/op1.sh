c7d83bd9967f63f0748e45cb2d093ca6feb64ff0
urever <83172845+urever@users.noreply.github.com>
2026-01-14 22:31:22 +0800

    Update package sources in op1.sh
    Removed passwall package sources and added extraipk source.

--- diy/mt798x/op1.sh
+++ diy/mt798x/op1.sh
@@ -3,6 +3,4 @@
 # DaoDao's script 
 #=================================================
 ##添加自己的插件库
-src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
-src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main
 echo -e "\nsrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk" >> feeds.conf.default
