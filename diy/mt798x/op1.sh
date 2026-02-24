5284a15a6a4e2465fb442938651d7b2a4ddb5f24
urever <83172845+urever@users.noreply.github.com>
2026-02-24 15:31:05 +0800

    Update op1.sh

--- diy/mt798x/op1.sh
+++ diy/mt798x/op1.sh
@@ -6,3 +6,4 @@
 #echo -e "\nsrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk" >> feeds.conf.default
 #sed -i '1i src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 #sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+#sed -i '1i src-git OpenAppFilter_packages https://github.com/destan19/OpenAppFilter.git;main' feeds.conf.default
