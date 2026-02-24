bcea13377fa256982eca5a123b71a037f1455eda
urever <83172845+urever@users.noreply.github.com>
2026-02-24 15:44:40 +0800

    Update op2.sh

--- diy/mt798x/op2.sh
+++ diy/mt798x/op2.sh
@@ -5,7 +5,7 @@
 
 
 ##配置IP
-#sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 ##New WiFi
 sed -i "s/ImmortalWrt-2.4G/AX6000/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
